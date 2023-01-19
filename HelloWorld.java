140e0d863a7d222e4f745e3bdfaa8b88af6cf9dd
Mr.J <wzhting@163.com>
2023-01-19 15:21:51 +0800

    2.0在第二行加了内容

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld{
-	public static void main(String[] args){
+	2.0-1 public static void main(String[] args){
 		System.out.pringln("2.0Hello World!");
 		System.out.pringln("2.0Hello World!22222");
 	}
