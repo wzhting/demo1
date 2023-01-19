403754dda40ae248fb484d58fc611d20dc31cfca
Mr.J <wzhting@163.com>
2023-01-19 15:18:46 +0800

    master修改了第一行

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,4 +1,4 @@
-public class HelloWorld{
+public class HelloWorld Master{
 	public static void main(String[] args){
 		System.out.pringln("2.0Hello World!");
 		System.out.pringln("2.0Hello World!22222");
