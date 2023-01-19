1c183615d31eae73686bb3626798f2ddce086fd2
Mr.J <wzhting@163.com>
2023-01-19 15:04:44 +0800

    2.0分支第一次提交

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld{
 	public static void main(String[] args){
-		System.out.pringln("Hello World!");
-		System.out.pringln("Hello World!22222");
+		System.out.pringln("2.0Hello World!");
+		System.out.pringln("2.0Hello World!22222");
 	}
 }
