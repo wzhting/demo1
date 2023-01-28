db65a2c9d5286d139547ba7efc63cb554cfce69f
Mr.J <wzhting@163.com>
2023-01-28 14:33:38 +0800

    two

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld{
 	public static void main(String[] args){
 		System.out.pringln("Hello World!");
-		System.out.pringln("Hello World!1");
+		System.out.pringln("Hello World!12");
 	}
 }
